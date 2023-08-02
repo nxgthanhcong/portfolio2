b75d6c7b684c4aeba31b2855278efbd19b3481c6
unknown <nxgthanhcong@gmail.com>
2023-08-03 01:45:33 +0700

    add phen button

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,4 +9,12 @@ const $ = document.querySelector.bind(document)
             header.classList[pageOffsetY > 0 ? 'add' : 'remove'](HEADER_ACTIVE)
         })
 
+        // moblie:  button toggle menu
+        const toggleBtn = $('.toggle-btn')
+        const mobileNavigation = $('.mobile-navigation')
+        toggleBtn.addEventListener('click', function () {
+            toggleBtn.classList.toggle('active')
+            mobileNavigation.classList.toggle('active')
+        })
+
     })()
